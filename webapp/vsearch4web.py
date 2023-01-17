af21578a7889809fa77d049de4ba50fb7467827d
Max <Nikolaev.max05@gmail.com>
2023-01-17 12:28:06 +0300

    new changes

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from vsearch import search4letters
 
-
 app = Flask(__name__)
 
 
@@ -14,8 +13,11 @@ def hello() -> str:
 def do_search() -> str:
     return str(search4letters('life, the universe, and everything', 'aeiou'))
 
+
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entru.html',
+    return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
+
+
 app.run()
