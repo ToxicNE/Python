d2a5f64f2950055448fd1d3741e6930cd324fa82
Max <Nikolaev.max05@gmail.com>
2023-01-24 12:19:59 +0300

    new changes

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -4,24 +4,25 @@ from vsearch import search4letters
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-
-
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
     phrase = request.form['phrase']
     letters = request.form['letters']
-    return str(search4letters(phrase, letters))
+    title = 'Here you results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results, )
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
 
 
-
-
+if__name__ = '__main__'
 app.run(debug=True)
